82ebafd542834854b124475093c6a981c8034f4d
Steven Wang <181110315@stu.hit.edu.cn>
2022-04-11 10:59:49 +0800

    update map

--- research-map.js
+++ research-map.js
@@ -1,10 +1,3 @@
-/*
- * :Author: Shaobo Wang
- * :Date: 2022-04-08 21:32:17
- * :LastEditors: Shaobo Wang
- * :LastEditTime: 2022-04-09 14:32:08
- */
-
 go.Shape.defineFigureGenerator("RoundedRectangle", function (shape, w, h) {
   // this figure takes one parameter, the size of the corner
   var p1 = 10; // default corner size
@@ -47,26 +40,28 @@ function init() {
   // For details, see https://gojs.net/latest/intro/buildingObjects.html
   const $ = go.GraphObject.make; // for conciseness in defining templates
 
-  myDiagram = $(
+  var diagram = $(
     go.Diagram,
-    "myDiagramDiv", // must be the ID or reference to div
+    "myDiagramDiv", // id of DIV
     {
-      allowCopy: false,
-      isReadOnly: true,
-      allowSelect: false,
-      allowDrop: false,
-      allowMove: false,
-      initialAutoScale: go.Diagram.Uniform,
-      // create a TreeLayout for the family tree
-      layout: $(go.TreeLayout, 
-        { angle: 90, 
-          layerSpacing : 70,
-          nodeSpacing: 0 
-        }),
-      // layout: $(go.TreeLayout, { nodeSpacing: 5 }),
+      autoScale: go.Diagram.UniformToFill,
+      initialContentAlignment: go.Spot.TopCenter,
+      layout: $(
+        go.TreeLayout,
+        {
+          angle: 90,
+          nodeSpacing: 10,
+          layerSpacing: 60,
+          layerStyle: go.TreeLayout.LayerSiblings,
+          compaction: go.TreeLayout.CompactionBlock,
+          alignment: go.TreeLayout.AlignmentCenterChildren,
+          treeStyle: go.TreeLayout.StyleLayered,
+          //   breadthLimit: 1,
+        }
+        // new go.Binding("nodeSpacing", "part", partSpaceConverter)
+      ),
     }
   );
-
   var bluegrad = $(go.Brush, "Linear", {
     0: "rgb(60, 204, 254)",
     1: "rgb(70, 172, 254)",
@@ -85,7 +80,7 @@ function init() {
   });
 
   // Set up a Part as a legend, and place it directly on the diagram
-  myDiagram.add(
+  diagram.add(
     $(
       go.Part,
       "Table",
@@ -158,10 +153,11 @@ function init() {
   }
   // define Converters to be used for Bindings
   function partSizeConverter(part) {
-    if (part === "F") return new go.Size(190, 100);
+    if (part === "F") return new go.Size(220, 100);
     if (part === "P") return new go.Size(180, 100);
-    if (part === "T") return new go.Size(130, 100);
-    return new go.Size(150, 140);
+    if (part === "T") return new go.Size(110, 100);
+    if (part === "M") return new go.Size(110, 120);
+    return new go.Size(110, 140);
   }
   // define Converters to be used for Fonts
   function partFontConverter(part) {
@@ -170,194 +166,137 @@ function init() {
     if (part === "T") return "6pt Helvetica,";
     return "6pt Helvetica, bold Arial, sans-serif";
   }
-
-  // replace the default Node template in the nodeTemplateMap
-  myDiagram.nodeTemplate = $(
+  function partSpaceConverter(part) {
+    if (part === "F") return 100;
+    if (part === "P") return 50;
+    if (part === "T") return 19;
+    return 10;
+  }
+  // Define a node template showing class names.
+  // Clicking on the node opens up the documentation for that class.
+  diagram.nodeTemplate = $(
     go.Node,
-    "Auto",
-    { deletable: false },
-    new go.Binding("text", "name"),
+    "Vertical",
+    {
+      location: new go.Point(100, 0), // set the Node.location
+      locationSpot: go.Spot.Center,
+    },
     $(
-      go.Shape,
-      "RoundedRectangle",
-      {
-        // desiredSize: new go.Size(200, 72),
-        desiredSize: new go.Size(130, 120),
-        fill: greengrad,
-        margin: 5,
-      },
-      new go.Binding("desiredSize", "part", partSizeConverter),
-      {
-        fill: greengrad,
-        stroke: "black",
-        stretch: go.GraphObject.Fill,
-        alignment: go.Spot.Center,
-      },
-      new go.Binding("fill", "part", partBrushConverter)
-    ),
-    $(
-      go.Panel,
-      "Auto",
-      //   $(
-      //     go.TextBlock,
-      //     {
-      //       font: "bold 8pt Helvetica, bold Arial, sans-serif",
-      //       alignment: go.Spot.Center,
-      //       margin: 6,
-      //     },
-      //     new go.Binding("text", "name")
-      //   ),
+      "HyperlinkText",
+      // compute the URL to open for the documentation
+      (node) => "#" + node.data.name,
+      // define the visuals for the hyperlink, basically the whole node:
       $(
-        "HyperlinkText",
-        (node) => "#" + node.data.name,
-        (node) => node.data.name,
-        {
-          margin: 5,
-          maxSize: new go.Size(200, 180),
-          // font: "8pt Helvetica, bold Arial, sans-serif",
-          textAlign: "center",
-        },
-        new go.Binding("text", "name"),
-        new go.Binding("font", "part", partFontConverter)
+        go.Panel,
+        "Auto",
+        $(
+          go.Shape,
+          "RoundedRectangle",
+          {
+            fill: greengrad,
+            stroke: "black",
+            stretch: go.GraphObject.Fill,
+            alignment: go.Spot.TopCenter,
+            // desiredSize: new go.Size(110, 120),
+          },
+          new go.Binding("fill", "part", partBrushConverter)
+        ),
+
+        $(
+          go.TextBlock,
+          {
+            // font: "bold 13px Helvetica, bold Arial, sans-serif",
+            stroke: "black",
+            margin: 3,
+            maxLines: 6,
+            width: 130,
+            textAlign: "center",
+            verticalAlignment: go.Spot.Center,
+            // wrap: go.TextBlock.WrapFit,
+          },
+          new go.Binding("desiredSize", "part", partSizeConverter),
+          new go.Binding("text", "name"),
+          new go.Binding("font", "part", partFontConverter)
+        )
       )
     )
   );
 
-  // define the Link template
-  myDiagram.linkTemplate = $(
-    go.Link, // the whole link panel
-    // { routing: go.Link.Orthogonal, corner: 5, selectable: false },
-    { corner: 5, selectable: true },
-    $(go.Shape)
-  ); // the default black link shape
-
+  // Define a trivial link template with no arrowhead
+  diagram.linkTemplate = $(
+    go.Link,
+    {
+      curve: go.Link.Bezier,
+      toEndSegmentLength: 30,
+      fromEndSegmentLength: 30,
+    },
+    $(go.Shape, { strokeWidth: 1.5 }) // the link shape, with the default black stroke
+  );
   var nodeDataArray = [
-    {
-      key: 1,
-      part: "F",
-      name: "Game-theoretic\ninteractions",
-    },
-    {
-      key: 2,
-      part: "P",
-      name: "Unifying different\nexplanations",
-    },
+    { key: 1, name: "Game-theoretic interactions", part: "F" },
+    { key: 2, name: "Unifying different explanations", part: "P" },
     {
       key: 3,
+      name: "Extracting the common mechanisms of different methods",
       part: "P",
-      name: "Extracting the\ncommon mechanisms\nof different methods",
     },
     {
       key: 4,
+      name: "Learning baseline values of Shapley value explanations",
       part: "T",
-      name: "Learning\nbaseline values\nof Shapley value\nexplanations",
     },
     {
       key: 5,
+      name: "Explain interactions between features in a DNN",
       part: "T",
-      name: "Explain interactions\nbetween features\nin a DNN",
     },
-    { key: 6, 
-      part: "T", 
-      name: "Explain and unify\nattribution\nexplanations" 
-    },
-    { key: 7, part: "T", name: "Explain the\nsemantic concepts\nof a DNN" },
+    { key: 6, name: "Explain and unify attribution explanations", part: "T" },
+    { key: 7, name: "Explain the semantic concepts of a DNN", part: "T" },
     {
       key: 8,
       part: "T",
-      name: "Discover and\nexplain the\nrepresentation\nbottleneck\nof DNNs",
+      name: "Discover and explain the representation bottleneck of DNNs",
     },
-    { key: 9, part: "T", name: "Explain the\ngeneralization\nability" },
-    { key: 10, part: "T", name: "Explain the\nadversarial\ntransferability" },
-    { key: 11, part: "T", name: "Explain the\naesthetic\nappreciation" },
-    { key: 12, part: "T", name: "Explain the\nadversarial\nrobustness" },
+    { key: 9, name: "Explain the generalization ability", part: "T" },
+    { key: 10, name: "Explain the adversarial transferability", part: "T" },
+    { key: 11, name: "Explain the aesthetic appreciation", part: "T" },
+    { key: 12, name: "Explain the adversarial robustness", part: "T" },
     {
       key: 13,
-      name: "Address the\ntheoretical flaws \nwith baseline values\nof Shapley values",
+      name: "\nAddress the theoretical flaws with baseline values of Shapley values\n",
     },
     {
       key: 14,
-      name: "Explain a DNN\nfrom interactions\nbetween features,\ninstead of the\nimportance of an\n individual feature",
+      name: "Explain a DNN from interactions between features, instead of the importance of an individual feature",
     },
     {
       key: 15,
-      name: "Attribution methods\nare based on\nvarious heuristics,\nlacking common\ntheoretical\n mechanisms",
+      name: "Attribution methods are based on various heuristics, lacking common theoretical mechanisms",
     },
     {
       key: 16,
-      name: "Provide a new\ntaxonomy of\nsemantic concepts\nw.r.t. complexities",
+      name: "\nProvide a new taxonomy of semantic concepts w.r.t. complexities\n",
     },
     {
       key: 17,
-      name: "Explore the\ncommon tendency\nof feature\nrepresentations\nof DNNs",
+      name: "\nExplore the common tendency of feature representations of DNNs\n",
     },
     {
       key: 18,
-      name: "Most techniques\nstrengthening the\ngeneralization\nabilities are\nheuristic",
+      name: "\nMost techniques strengthening the generalization abilities are heuristic\n",
     },
     {
       key: 19,
-      name: "Current studies of\nboosting adversarial\ntransferability are\nmainly based\non heuristics and\nobservasions",
+      name: "Current studies to explore factors influencing the transferability are based on experiments",
     },
     {
       key: 20,
-      name: "Existing works lack\na  mathematical\nmodeling for\nthe aesthetic\nappreciation",
+      name: "Existing works lack a mathematical modeling for the aesthetic appreciation\n",
     },
     {
       key: 21,
-      name: "The essence of\nadversarial attack\nand robustness\nis still unclear",
+      name: "Many techniques boosting the robustness are heuristic, whose mechanisms are largely missing",
     },
-    // { key: 1, name: "Game-theoretic interactions" },
-    // { key: 2, name: "Unifying different explanations" },
-    // { key: 3, name: "Extracting the common mechanisms of different methods" },
-    // { key: 4, name: "Learning baseline values of Shapley value explanations" },
-    // { key: 5, name: "Explain interactions between features in a DNN" },
-    // { key: 6, name: "Explain and unify attribution explanations" },
-    // { key: 7, name: "Explain the semantic concepts of a DNN" },
-    // {
-    //   key: 8,
-    //   name: "Discover and explain the representation bottleneck of DNNs",
-    // },
-    // { key: 9, name: "Explain the generalization ability" },
-    // { key: 10, name: "Explain the adversarial transferability" },
-    // { key: 11, name: "Explain the aesthetic appreciation" },
-    // { key: 12, name: "Explain the adversarial robustness" },
-    // {
-    //   key: 13,
-    //   name: "Address the theoretical flaws with baseline values of Shapley values",
-    // },
-    // {
-    //   key: 14,
-    //   name: "Explain a DNN from interactions between features, instead of the importance of an individual feature",
-    // },
-    // {
-    //   key: 15,
-    //   name: "Attribution methods are based on various heuristics, lacking common theoretical mechanisms",
-    // },
-    // {
-    //   key: 16,
-    //   name: "Provide a new taxonomy of semantic concepts w.r.t. complexities",
-    // },
-    // {
-    //   key: 17,
-    //   name: "Explore the common tendency of feature representations of DNNs",
-    // },
-    // {
-    //   key: 18,
-    //   name: "Most techniques strengthening the generalization abilities are heuristic",
-    // },
-    // {
-    //   key: 19,
-    //   name: "Current studies to explore factors influencing the transferability are based on experiments",
-    // },
-    // {
-    //   key: 20,
-    //   name: "Existing works lack a  mathematical modeling for the aesthetic appreciation",
-    // },
-    // {
-    //   key: 21,
-    //   name: "Many techniques boosting the robustness are heuristic, whose mechanisms are largely missing",
-    // },
   ];
   var linkDataArray = [
     { from: 1, to: 2 },
@@ -385,57 +324,8 @@ function init() {
     { from: 11, to: 20 },
     { from: 12, to: 21 },
   ];
-  myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
+
+  // Create the model for the hierarchy diagram
+  diagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
 }
 window.addEventListener("DOMContentLoaded", init);
-// var $ = go.GraphObject.make;
-// myDiagram = $(go.Diagram, "myDiagramDiv", {
-//   contentAlignment: go.Spot.TopCenter,
-//   isReadOnly: true,
-//   allowSelect: false,
-//   allowDrop: false,
-//   allowMove: false,
-// });
-
-// myDiagram.nodeTemplate = $(
-//   go.Node,
-//   "Auto",
-//   $(go.Shape, "Ellipse", { fill: "lightskyblue" }),
-//   $(
-//     "HyperlinkText",
-//     (node) => "#" + node.data.name,
-//     (node) => node.data.name,
-//     { margin: 5, maxSize: new go.Size(200, 150), textAlign: "center" }
-//   )
-// );
-// myDiagram.layout = $(go.TreeLayout, { layerSpacing: 50, nodeSpacing: 10 });
-// myDiagram.model = new go.TreeModel([
-//   { key: "1", name: "Game-theoretic interactions" },
-//   { key: "2", parent: "1", name: "Unifying different explanations" },
-//   {
-//     key: "3",
-//     parent: "1",
-//     name: "Extracting the common mechanisms of different methods",
-//   },
-//   {
-//     key: "4",
-//     parent: "2",
-//     name: "Learning baseline values of Shapley value explanations",
-//   },
-//   {
-//     key: "5",
-//     parent: "2",
-//     name: "Explain interactions between features in a DNN",
-//   },
-//   { key: "6", parent: "2", name: "Explain and unify attribution explanations" },
-//   { key: "7", parent: "2", name: "Explain the semantic concepts of a DNN" },
-//   {
-//     key: "8",
-//     parent: "2",
-//     name: "Discover and explain the representation bottleneck of DNNs",
-//   },
-//   { key: "9", parent: "2", name: "Explain the adversarial transferability" },
-//   { key: "10", parent: "2", name: "Explain the aesthetic appreciation" },
-//   { key: "11", parent: "2", name: "Explain the adversarial robustness" },
-//   { key: "12", parent: "2", name: "Explain the adversarial transferability" },
-// ]);
